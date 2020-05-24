d7003f97f43e07c4d1519611f0c02757439b1b63
MD EMRUL EMRAN <emrulhasan@gmail.com>
2020-05-24 01:56:49 -0400

    Create ReadFileContent.scala

---
+++ ReadFileContent.scala
@@ -0,0 +1,10 @@
+package inout
+
+import scala.io.Source
+
+object ReadFileContent {
+  def main(args: Array[String]) {
+    println("Following is the content read:")
+    Source.fromFile("C:\\Users\\Desktop\\output\\Test.txt").foreach(print)
+  }
+}
