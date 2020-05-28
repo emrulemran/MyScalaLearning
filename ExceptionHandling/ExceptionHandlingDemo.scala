5ca0037908cf0cd42d54f3b5d9ec014e47940270
MD EMRUL EMRAN <emrulhasan@gmail.com>
2020-05-28 11:55:46 -0400

    Create ExceptionHandlingDemo.scala

---
+++ ExceptionHandling/ExceptionHandlingDemo.scala
@@ -0,0 +1,27 @@
+package practice
+
+class ExceptionHandlingDemo {
+  def div(x: Int, y: Int) {
+    
+    
+  
+    try {
+      x / y
+    } catch {
+      case e: ArithmeticException => println(e)
+    }
+    println("Rest of the code is executing . . .")
+  }
+}
+
+object EH {
+  def main(args: Array[String]): Unit = {
+
+    var ob = new ExceptionHandlingDemo()
+
+    ob.div(100, 0)
+    
+    println("A tale that never ends . . . ")
+  }
+
+}
