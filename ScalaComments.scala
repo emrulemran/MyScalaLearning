a21fce452e6fff97e30bafba5f3ef0f841887e10
MD EMRUL EMRAN <emrulhasan@gmail.com>
2020-05-29 17:17:28 -0400

    Create ScalaComments.scala

---
+++ ScalaComments.scala
@@ -0,0 +1,27 @@
+package practice
+
+object ScalaComments {
+  def main(args: Array[String]): Unit = {
+    // this is a SINGLE-LINE-COMMENT
+    var a = 1
+
+    var b = 123 // This is an END-OF-LINE COMMENT
+
+    /* THIS IS A MULTI LINE COMMENT
+     * this is a multi line comment
+     * this is a multi line comment
+     * this is a multi line comment
+     * this is a multi line comment
+     * this is a multi line comment
+     */
+
+    val x = 25
+    println(x)
+    /**
+     * THIS IS A DOCUMENTATION COMMENT:
+     *  In the main method, we have created a variable named a
+     *  and printed it
+     */
+
+  }
+}
