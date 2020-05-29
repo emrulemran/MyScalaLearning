6828af8920752d9b17e766e3c8e11c4784520b60
MD EMRUL EMRAN <emrulhasan@gmail.com>
2020-05-29 17:03:53 -0400

    Create MethodOverloading1.scala

---
+++ MethodOverloading1.scala
@@ -0,0 +1,28 @@
+package practice
+
+class MethodOverloading1 {
+
+  def add(a: Int) {
+    var sum = a + a
+    println(sum)
+  }
+
+  def add(a: Int, b: Int) {
+    var sum = a + b
+    println(sum)
+  }
+
+  def add(a: Int, b: Int, c: Int) {
+    var sum = a + b + c
+    println(sum)
+  }
+}
+
+object MO {
+  def main(args: Array[String]) {
+    var a = new MethodOverloading();
+    a.add(25)
+    a.add(10, 10);
+    a.add(10, 10, 10);
+  }
+}  
