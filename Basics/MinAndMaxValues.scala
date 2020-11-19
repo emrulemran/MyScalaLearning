44ab1b06e7a3514cc938963e5944d8633b9ffd3d
MD EMRUL EMRAN <emrulhasan@gmail.com>
2020-11-18 20:24:04 -0600

    Create MinAndMaxValues.scala

---
+++ Basics/MinAndMaxValues.scala
@@ -0,0 +1,40 @@
+package class14
+
+object MinAndMaxValues {
+  def main(args: Array[String]): Unit = {
+    // printing max and min values for each of the numeric data types:
+    println(Byte.MaxValue)
+    println(Byte.MinValue)
+    
+    println(Short.MaxValue)
+    println(Short.MinValue)
+
+    println(Int.MaxValue)
+    println(Int.MinValue)
+
+    println(Long.MaxValue)
+    println(Long.MinValue)
+
+    println(Float.MaxValue)
+    println(Float.MinValue)
+
+    println(Double.MaxValue)
+    println(Double.MinValue)
+
+  }
+}
+
+
+Output:
+127
+-128
+32767
+-32768
+2147483647
+-2147483648
+9223372036854775807
+-9223372036854775808
+3.4028235E38
+-3.4028235E38
+1.7976931348623157E308
+-1.7976931348623157E308
